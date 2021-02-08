7baa1a0e6b87c9b290e7d54a56753a67005ec820
Gabe <ghuinker@gmail.com>
2021-02-08 12:53:37 -0600

    Add cacheing for elbow

--- app/algos/__init__.py
+++ app/algos/__init__.py
@@ -9,15 +9,28 @@ from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 from sklearn.preprocessing import StandardScaler
 
+from django.core.cache import cache
+
 def example_algo():
     """ Return the mass_1 of example_query nodes """
     nodes = Node.objects.example_query()
     masses = [node.mass_1 for node in nodes]
     return masses
 
+
+def get_elbow_cache_key(node_keys: list):
+    """Given a list of string return a unique cache key"""
+    return "elb_{}".format(hash(''.join(node_keys)))
+
 # Help from: https://realpython.com/k-means-clustering-python/
 def get_number_of_clusters(node_keys=['mass0_1', 'lumin_1']):
     """ Uses the elbow method to determine the best number of clusters for given data"""
+    # Check if elbow for node_keys exists
+    key = get_elbow_cache_key(node_keys)
+    elbow = cache.get(key)
+    if elbow:
+        return elbow
+    # Find the new elbow
     features = Node.objects.values_list(*node_keys)
     scaler = StandardScaler()
     scaled_features = scaler.fit_transform(features)
@@ -40,8 +53,9 @@ def get_number_of_clusters(node_keys=['mass0_1', 'lumin_1']):
         kmeans = KMeans(n_clusters=k, **kmeans_kwargs)
         kmeans.fit(scaled_features)
         sse.append(kmeans.inertia_)
-        plt.style.use("fivethirtyeight")
     kl = KneeLocator(
         range(1, 11), sse, curve="convex", direction="decreasing"
     )
+    # Add this elbow to cache
+    cache.set(key, kl.elbow, None)
     return kl.elbow
--- app/views.py
+++ app/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-from app.algos import example_algo, gabe
+from app.algos import example_algo
 
 # Create your views here.
 def index(request, *args, **kwargs):
     context = {
         'example_data': example_algo()[:10]
     }
-    gabe.run()
     return render(request, 'index.html', context)
