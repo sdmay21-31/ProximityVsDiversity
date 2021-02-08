43cf9bb5ec60f87c7348e2354abf39dd8931f57a
Gabe <ghuinker@gmail.com>
2021-02-08 13:38:09 -0600

    Create an initial clustering example using our data with kmeans

--- app/algos/gabe.py
+++ app/algos/gabe.py
@@ -1,25 +1,28 @@
-from app.models import Node
-from sklearn.cluster import KMeans
-from sklearn.datasets import make_blobs
+import tarfile
+import urllib
+
 import numpy as np
-
 import matplotlib.pyplot as plt
-from kneed import KneeLocator
-from sklearn.datasets import make_blobs
-from sklearn.cluster import KMeans
-from sklearn.metrics import silhouette_score
-from sklearn.preprocessing import StandardScaler
+import pandas as pd
+import seaborn as sns
 
-# mass0_1
-# lumin_1
-def run(*args):
-    features, true_labels = make_blobs(
-        n_samples=200,
-        centers=3,
-        cluster_std=2.75,
-        random_state=42
-    )
-    features = Node.objects.values_list('mass0_1', 'lumin_1')
+from app.models import Node
+
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from sklearn.metrics import silhouette_score, adjusted_rand_score
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler
+
+def get_number_of_clusters(node_keys=['mass0_1', 'lumin_1']):
+    """ Uses the elbow method to determine the best number of clusters for given data"""
+    # Check if elbow for node_keys exists
+    key = get_elbow_cache_key(node_keys)
+    elbow = cache.get(key)
+    if elbow:
+        return elbow
+    # Find the new elbow
+    features = Node.objects.values_list(*node_keys)
     scaler = StandardScaler()
     scaled_features = scaler.fit_transform(features)
     kmeans = KMeans(
@@ -36,14 +39,114 @@ def run(*args):
         "random_state": 42,
     }
 
-    # A list holds the SSE values for each k
     sse = []
     for k in range(1, 11):
         kmeans = KMeans(n_clusters=k, **kmeans_kwargs)
         kmeans.fit(scaled_features)
         sse.append(kmeans.inertia_)
-        plt.style.use("fivethirtyeight")
     kl = KneeLocator(
         range(1, 11), sse, curve="convex", direction="decreasing"
     )
-    print(kl.elbow)
+    # Add this elbow to cache
+    cache.set(key, kl.elbow, None)
+    return kl.elbow
+
+
+
+datafile = "TCGA-PANCAN-HiSeq-801x20531/data.csv"
+labels_file = "TCGA-PANCAN-HiSeq-801x20531/labels.csv"
+
+data = np.genfromtxt(
+    datafile,
+    delimiter=",",
+    usecols=range(1, 20532),
+    skip_header=1
+)
+
+true_label_names = np.genfromtxt(
+    labels_file,
+    delimiter=",",
+    usecols=(1,),
+    skip_header=1,
+    dtype="str"
+)
+
+label_encoder = LabelEncoder()
+
+true_labels = label_encoder.fit_transform(true_label_names)
+
+true_labels[:5]
+
+label_encoder.classes_
+
+node_keys=['mass0_1', 'lumin_1']
+data = Node.objects.values_list(*node_keys)[:801]
+
+n_clusters = 4
+
+preprocessor = Pipeline(
+    [
+        ("scaler", MinMaxScaler()),
+        ("pca", PCA(n_components=2, random_state=42)),
+    ]
+)
+
+clusterer = Pipeline(
+   [
+       (
+           "kmeans",
+           KMeans(
+               n_clusters=n_clusters,
+               init="k-means++",
+               n_init=50,
+               max_iter=500,
+               random_state=42,
+           ),
+       ),
+   ]
+)
+
+pipe = Pipeline(
+    [
+        ("preprocessor", preprocessor),
+        ("clusterer", clusterer)
+    ]
+)
+
+pipe.fit(data)
+
+preprocessed_data = pipe["preprocessor"].transform(data)
+
+predicted_labels = pipe["clusterer"]["kmeans"].labels_
+
+silhouette_score(preprocessed_data, predicted_labels)
+
+adjusted_rand_score(true_labels, predicted_labels)
+
+pcadf = pd.DataFrame(
+    pipe["preprocessor"].transform(data),
+    columns=["component_1", "component_2"],
+)
+
+pcadf["predicted_cluster"] = pipe["clusterer"]["kmeans"].labels_
+pcadf["true_label"] = label_encoder.inverse_transform(true_labels)
+
+plt.style.use("fivethirtyeight")
+plt.figure(figsize=(8, 8))
+
+scat = sns.scatterplot(
+    "component_1",
+    "component_2",
+    s=50,
+    data=pcadf,
+    hue="predicted_cluster",
+    style="true_label",
+    palette="Set2",
+)
+
+scat.set_title(
+    "Clustering of space dirt"
+)
+plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.0)
+
+plt.show()
