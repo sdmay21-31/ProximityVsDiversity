86d8bc217df03bd5b9e3bdb767ef7a577b86171d
Gabe <ghuinker@gmail.com>
2021-02-09 15:21:37 -0600

    Get a working example of embedding matplotlib

--- app/algos/gabe.py
+++ app/algos/gabe.py
@@ -1,8 +1,9 @@
 import tarfile
 import urllib
-
+import base64
+import io
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib
 import pandas as pd
 import seaborn as sns
 
@@ -14,139 +15,95 @@ from sklearn.metrics import silhouette_score, adjusted_rand_score
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 
-def get_number_of_clusters(node_keys=['mass0_1', 'lumin_1']):
-    """ Uses the elbow method to determine the best number of clusters for given data"""
-    # Check if elbow for node_keys exists
-    key = get_elbow_cache_key(node_keys)
-    elbow = cache.get(key)
-    if elbow:
-        return elbow
-    # Find the new elbow
-    features = Node.objects.values_list(*node_keys)
-    scaler = StandardScaler()
-    scaled_features = scaler.fit_transform(features)
-    kmeans = KMeans(
-        init="random",
-        n_clusters=3,
-        n_init=10,
-        max_iter=300,
-        random_state=42
+
+matplotlib.use('agg') #noq
+plt = matplotlib.pyplot
+
+
+
+def run(values):
+    true_label_names = ['one' for i in range(801)]
+
+    label_encoder = LabelEncoder()
+
+    true_labels = label_encoder.fit_transform(true_label_names)
+
+    true_labels[:5]
+
+    label_encoder.classes_
+
+    node_keys=['mass0_1', 'lumin_1']
+    data = Node.objects.values_list(*node_keys)[:801]
+
+    n_clusters = 4
+
+    preprocessor = Pipeline(
+        [
+            ("scaler", MinMaxScaler()),
+            ("pca", PCA(n_components=2, random_state=42)),
+        ]
     )
-    kmeans_kwargs = {
-        "init": "random",
-        "n_init": 10,
-        "max_iter": 300,
-        "random_state": 42,
-    }
 
-    sse = []
-    for k in range(1, 11):
-        kmeans = KMeans(n_clusters=k, **kmeans_kwargs)
-        kmeans.fit(scaled_features)
-        sse.append(kmeans.inertia_)
-    kl = KneeLocator(
-        range(1, 11), sse, curve="convex", direction="decreasing"
-    )
-    # Add this elbow to cache
-    cache.set(key, kl.elbow, None)
-    return kl.elbow
-
-
-
-datafile = "TCGA-PANCAN-HiSeq-801x20531/data.csv"
-labels_file = "TCGA-PANCAN-HiSeq-801x20531/labels.csv"
-
-data = np.genfromtxt(
-    datafile,
-    delimiter=",",
-    usecols=range(1, 20532),
-    skip_header=1
-)
-
-true_label_names = np.genfromtxt(
-    labels_file,
-    delimiter=",",
-    usecols=(1,),
-    skip_header=1,
-    dtype="str"
-)
-
-label_encoder = LabelEncoder()
-
-true_labels = label_encoder.fit_transform(true_label_names)
-
-true_labels[:5]
-
-label_encoder.classes_
-
-node_keys=['mass0_1', 'lumin_1']
-data = Node.objects.values_list(*node_keys)[:801]
-
-n_clusters = 4
-
-preprocessor = Pipeline(
-    [
-        ("scaler", MinMaxScaler()),
-        ("pca", PCA(n_components=2, random_state=42)),
-    ]
-)
-
-clusterer = Pipeline(
-   [
-       (
-           "kmeans",
-           KMeans(
-               n_clusters=n_clusters,
-               init="k-means++",
-               n_init=50,
-               max_iter=500,
-               random_state=42,
+    clusterer = Pipeline(
+       [
+           (
+               "kmeans",
+               KMeans(
+                   n_clusters=n_clusters,
+                   init="k-means++",
+                   n_init=50,
+                   max_iter=500,
+                   random_state=42,
+               ),
            ),
-       ),
-   ]
-)
+       ]
+    )
 
-pipe = Pipeline(
-    [
-        ("preprocessor", preprocessor),
-        ("clusterer", clusterer)
-    ]
-)
+    pipe = Pipeline(
+        [
+            ("preprocessor", preprocessor),
+            ("clusterer", clusterer)
+        ]
+    )
 
-pipe.fit(data)
+    pipe.fit(data)
 
-preprocessed_data = pipe["preprocessor"].transform(data)
+    preprocessed_data = pipe["preprocessor"].transform(data)
 
-predicted_labels = pipe["clusterer"]["kmeans"].labels_
+    predicted_labels = pipe["clusterer"]["kmeans"].labels_
 
-silhouette_score(preprocessed_data, predicted_labels)
+    silhouette_score(preprocessed_data, predicted_labels)
 
-adjusted_rand_score(true_labels, predicted_labels)
+    adjusted_rand_score(true_labels, predicted_labels)
 
-pcadf = pd.DataFrame(
-    pipe["preprocessor"].transform(data),
-    columns=["component_1", "component_2"],
-)
+    pcadf = pd.DataFrame(
+        pipe["preprocessor"].transform(data),
+        columns=["component_1", "component_2"],
+    )
 
-pcadf["predicted_cluster"] = pipe["clusterer"]["kmeans"].labels_
-pcadf["true_label"] = label_encoder.inverse_transform(true_labels)
+    pcadf["predicted_cluster"] = pipe["clusterer"]["kmeans"].labels_
+    pcadf["true_label"] = label_encoder.inverse_transform(true_labels)
 
-plt.style.use("fivethirtyeight")
-plt.figure(figsize=(8, 8))
+    plt.style.use("fivethirtyeight")
+    plt.figure(figsize=(8, 8))
 
-scat = sns.scatterplot(
-    "component_1",
-    "component_2",
-    s=50,
-    data=pcadf,
-    hue="predicted_cluster",
-    style="true_label",
-    palette="Set2",
-)
+    scat = sns.scatterplot(
+        "component_1",
+        "component_2",
+        s=50,
+        data=pcadf,
+        hue="predicted_cluster",
+        style="true_label",
+        palette="Set2",
+    )
 
-scat.set_title(
-    "Clustering of space dirt"
-)
-plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.0)
-
-plt.show()
+    scat.set_title(
+        "Clustering of space dirt"
+    )
+    fig = plt.gcf()
+    buf = io.BytesIO()
+    fig.savefig(buf,format='png')
+    buf.seek(0)
+    string = base64.b64encode(buf.read())
+    uri = urllib.parse.quote(string)
+    return uri
--- app/views.py
+++ app/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from app.algos import example_algo
+from app.algos import example_algo, gabe
 
 # Create your views here.
 def index(request, *args, **kwargs):
     context = {
         'example_data': example_algo()[:10]
     }
+    context['data'] = gabe.run(['mass0_1', 'lumin_1'])
     return render(request, 'index.html', context)
