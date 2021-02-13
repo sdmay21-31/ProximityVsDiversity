d25429ec3e14d7c2b86d839a59435a54544b22e4
Jacob Spooner <jacobspooner92@gmail.com>
2021-02-13 17:37:57 -0600

    Have Node objects displayed in the form

--- app/forms.py
+++ app/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from app.models import Node
 
 class AlgoRequestForm(forms.Form):
-    attribute1 = forms.ModelMultipleChoiceField(queryset=Node.objects.all())
+    attribute1 = forms.ChoiceField(widget=forms.Select, choices=Node.objects.all())
     attribute2 = forms.ModelMultipleChoiceField(queryset=Node.objects.all())
     attribute3 = forms.ModelMultipleChoiceField(queryset=Node.objects.all())
     
