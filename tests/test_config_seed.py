c13a9afc0e1d8d1a507b7c6c7283ded623739a59
bhuinker <bhuinker@iastate.edu>
2021-03-21 22:58:11 -0500

    Tried to fix command line unit tests, but did not yet succeed

--- tests/test_config_seed.py
+++ tests/test_config_seed.py
@@ -1,4 +1,7 @@
 from app import seed
+from subprocess import call
+import os
+import django
 
 # TEST_URL = 'tests/test_datasets/'
 
@@ -8,15 +11,60 @@ from app.seed import nodeDictionary
 
 class TestConfigSeed:
     
-    # TODO
-    def dummy_test(self, db):
-        database = 'main_table_1.csv'
+    def test_without_database_arg(self, db):
+        database = "main_table_1.csv"
         
-        # Delete all rows
-        node_to_delete = nodeDictionary[database]
-        node_to_delete.objects.all().delete()
+        # Django is definitely installed...What is the problem???
+        print(django.VERSION)
         
-        seed.run(database)
+        print(os.getcwd())
         
+        # This may not be a valid function call...
+        #call(["source", "../.venv/Scripts/activate"])
+        
+        # The path for manage.py depends on the current working directory
+        argList = ["python", "manage.py", "seed"]
+        #argList = ["python", "../manage.py", "seed"]
+        
+        # Setting python path does not appear to work, unless wrong directory was used
+        # DJANGO_SETTINGS_MODULE = config.settings -> settings.py
+        
+        # manage.py cannot find "django" for some reason on line 11
+        # Some item failed to be imported at the very least...
+        call(argList)
+        print(argList)
+        
+        node_type = nodeDictionary[database]
         count = node_type.objects.count()
-        assert count == 10355968
+        assert count == 10000
+    
+    def test_database_arg_1(self, db):
+        database = "main_table_1.csv"
+        
+        argList = ["python", "manage.py", "seed", "-d", database]
+        call(argList)
+        
+        node_type = nodeDictionary[database]
+        count = node_type.objects.count()
+        assert count == 10000
+    
+    def test_database_arg_2(self, db):
+        database = "main_table_2.csv"
+        
+        argList = ["python", "manage.py", "seed", "-d", database]
+        call(argList)
+        
+        node_type = nodeDictionary[database]
+        count = node_type.objects.count()
+        assert count == 10000
+    
+    def test_database_arg_3(self, db):
+        database = "hungary_chickenpox.csv"
+        
+        argList = ["python", "manage.py", "seed", "-d", database]
+        call(argList)
+        
+        node_type = nodeDictionary[database]
+        count = node_type.objects.count()
+        assert count == 10000
+    
