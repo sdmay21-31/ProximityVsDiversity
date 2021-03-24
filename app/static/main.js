0f327e2cdbfd402379bf00bf65fa8443c0c05a74
k98kumar <k98kumar@gmail.com>
2021-03-24 12:13:01 -0500

    Some minor changes. Put in the base code for plotly using json.

--- app/static/main.js
+++ app/static/main.js
@@ -319,8 +319,8 @@ function process() {
       }
       referenceImg.src = `data:image/png;base64,${json.chart}`;
       document.querySelector(".chart-container").appendChild(chart);
-      // if (json.dataFile) {
-    	//   renderChart(json.dataFile);
+      // if (json.plotlyData) {
+    	//   renderPlotly(json.plotlyData);
       // }
       notyf.success("Processing successful.");
     }).catch(function(reason) {
@@ -372,18 +372,51 @@ function createSummary(data, proxWeights, divWeights) {
 /**
  * TODO: render chart
  */
-function renderChart(dataFile) {
-  // TODO: Render json with json or csv
-  // Need to receive a file from POST
-  // Plotly.d3.json();
-  // Plotly.plot( document.querySelector(".plotly-container"), [{
-  //   x: [1, 2, 3, 4, 5],
-  //   y: [1, 2, 4, 8, 16]
-  // }], { 
-  //   margin: { t: 0 }
-  // }, {
-  //   showSendToCloud:true
-  // });
+function renderPlotly(plotlyData) {
+  function unpack(rows, key) {
+    const ind = dummydata_columns.indexOf(key);
+    return rows.map( function(row) { return row[ind]; } );
+  }
+  const trace1 = {
+    x:unpack(plotlyData, 'x1'), y: unpack(plotlyData, 'y1'), z: unpack(plotlyData, 'z1'),
+    mode: 'markers',
+    marker: {
+      size: 12,
+      line: {
+        color: 'rgba(217, 217, 217, 0.14)',
+        width: 0.5
+      },
+      opacity: 0.8
+    },
+    type: 'scatter3d'
+  };
+  const trace2 = {
+    x:unpack(plotlyData, 'x2'), y: unpack(plotlyData, 'y2'), z: unpack(plotlyData, 'z2'),
+    mode: 'markers',
+    marker: {
+      color: 'rgb(127, 127, 127)',
+      size: 12,
+      symbol: 'circle',
+      line: {
+        color: 'rgb(204, 204, 204)',
+        width: 1
+      },
+      opacity: 0.8
+    },
+    type: 'scatter3d'
+  };
+  const data = [trace1, trace2];
+  // const data = [trace2];
+  const layout = {
+    margin: { l: 0, r: 0, b: 0, t: 0 },
+    scene: {
+      xaxis: { title: 'x Attribute' },
+      yaxis: { title: 'y Attribute' },
+      zaxis: { title: 'z Attribute' }
+    }
+  };
+  const config = { responsive: true };
+  Plotly.newPlot(document.querySelector(".plotly-container"), data, layout, config);
 }
 function exit(status) {
   // https://stackoverflow.com/a/550583
@@ -426,4 +459,6 @@ function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
 fetchDBsAndPopulateDropdown();
-renderChart("something");
+// (async function() {
+//   renderPlotly(dummydata_data);
+// })();
