d98feee60d3ac8a9bb0aa5f8e5ca09fce026b58a
Gabe <ghuinker@gmail.com>
2021-04-10 14:31:20 -0500

    Add debugging

--- config/settings.py
+++ config/settings.py
@@ -140,5 +140,22 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = 'static'
 
-# Mimetypes
-# mimetypes.add_type("text/css", ".css", True)
+if not DEBUG:
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'handlers': {
+            'file': {
+                'level': 'ERROR',
+                'class': 'logging.FileHandler',
+                'filename': os.path.join(BASE_DIR, 'logs', 'errors.log'),
+            },
+        },
+        'loggers': {
+            'django': {
+                'handlers': ['file'],
+                'level': 'ERROR',
+                'propagate': True,
+            },
+        },
+    }
